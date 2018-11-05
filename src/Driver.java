b5f83c33d0122c6bc914168ede1fc680f380f946
Brian Intile <brianintile@gmail.com>
2018-11-04 21:01:51 -0500

    Update driver output to be more descriptive

--- src/Driver.java
+++ src/Driver.java
@@ -12,7 +12,8 @@ public class Driver
 		SDES sdes = new SDES();
 
 		Scanner scanner = new Scanner (System.in);
-		
+
+		System.out.println ("Enter your 10-bit key");
 		sdes.getKey10(scanner);
 
 		String plain = "x";
@@ -24,7 +25,7 @@ public class Driver
 			cipher = sdes.encrypt  (plain);
 			System.out.print ("Cipher is ");
 			sdes.show (cipher);
-			System.out.println (sdes.byteArrayToString (sdes.decrypt (cipher)));
+			System.out.println ("Decryption of cipher:\n" + sdes.byteArrayToString (sdes.decrypt (cipher)));
 			System.out.println ("Enter plain text, or hit 'Enter' to terminate");
 			plain = scanner.nextLine();
 		}
